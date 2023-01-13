c2e53ed055baa92739ec79f4392858341c932e9b
haikalarif <haikal.arif164@gmail.com>
2023-01-13 14:05:01 +0700

    buat-tabel-di-database

--- database/migrations/2023_01_13_040751_create_clients_table.php
+++ database/migrations/2023_01_13_040751_create_clients_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('client', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('kontak');
+            $table->foreignId('id_project');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('client');
     }
 };
